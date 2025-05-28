eab9d2f4f4301fd9d57c87446e7824c2f973418b
Triet Vo <tvo@roblox.com>
2025-05-28 02:44:32 -0700

    tweak

--- game.js
+++ game.js
@@ -40,9 +40,9 @@ class FlappyBird {
             y: this.canvas.height / 3, // Start 1/3 from the top
             width: birdWidth,
             height: birdHeight,
-            gravity: 0.09, // Slower gravity
+            gravity: 0.12, // Slower gravity
             velocity: 0,
-            jump: -5, // Slightly less bouncy jump
+            jump: -4, // Slightly less bouncy jump
             hitboxPadding: 0.2 // 20% padding on all sides
         };
         
